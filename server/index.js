fd55c653ff40008197cbe44248e9cdb05009d13d
Matt Brucker <mpbrucker@gmail.com>
2018-08-07 23:00:41 -0400

    Adding dotenv compatibility.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import fs from 'fs';
 const app = express();
+
+// Handle environment file
+if (fs.exists('./.env')) {
+    dotenv.config();
+}
 
 
 // Regex to match on GET requests from component URIs
